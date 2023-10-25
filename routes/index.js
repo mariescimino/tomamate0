0bf1026f87e0f0f087c10a5237208cf781285701
mariescimino <mariescimino@outlook.com>
2023-10-25 15:31:51 -0300

    proyecto

---
+++ routes/index.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer= require('nodemailer'); 
+var novedadesModel= require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+
+  var novedades= await novedadesModel.getNovedades();
+  
+  novedades = novedades.splice(0,20);
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: '/images/noimage.jpg'
+      }
+    }
+  });
+
+  res.render('index', { novedades });
+});
+
+router.post('/', async(req, res, next)=>{
+  
+  console.log(req.body) //estoy cpaturando los datos?
+
+  var nombre= req.body.nombre;
+  var apellido= req.body.apellido;
+  var email= req.body.email;
+  var tel= req.body.tel;
+  var mensaje= req.body.mensaje;
+
+  var obj= {
+    to:'mariescimino@gmail.com',
+    subject: 'Contacto desde la web',
+    html: nombre + " " + apellido + " se contacta a traves y quiere mas info a este correo: " + email + ". <br> Ademas, hizo el siguiente mensaje: " + mensaje +". <br> Su tel es " + tel
+  }
+  
+  var transporter= nodemailer.createTransport ({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+ 
+  })
+  var info = await transporter.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'  
+  })
+
+}) // cierro peticion de post 
+
+module.exports = router;
